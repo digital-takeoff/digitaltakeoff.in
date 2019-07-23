b86d8fb87f26c20e3c31dfdea2f5bc1e5057ed32
digitaltakeoff20 <53189040+digitaltakeoff20@users.noreply.github.com>
2019-07-23 22:58:39 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="css/style.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+			integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+			crossorigin="anonymous"></script>
+    <title>Digital Takeoff</title>
+</head>
+<body>
+    <div class="dt01">
+        <div class="dt01_01">
+            <?php include 'SVG/Asset1.svg'; ?>
+        </div>
+        <div class="dt01_03">
+            <img src="1x/Asset 5.png" alt="text" />
+        </div>
+        <div class="dt01_02">
+            <ul>
+                <li id="github">
+                    <span>
+                        <i class="fab fa-github"></i>
+                    </span>
+                </li>
+                <li id="linkedin">
+                    <span>
+                        <i class="fab fa-linkedin"></i>
+                    </span>
+                </li>
+                <li id="instagram">
+                    <span>
+                        <i class="fab fa-instagram"></i>
+                    </span>
+                </li>
+                <li id="facebook">
+                    <span>
+                        <i class="fab fa-facebook-square"></i>
+                    </span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
